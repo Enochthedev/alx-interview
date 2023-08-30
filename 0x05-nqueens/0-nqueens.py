952dc23f8317f9de7b9bb36c2a29d17a3a6497fb
Toluthedev <enochjesse884@gmail.com>
2023-08-30 11:50:37 +0100

    minor change

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -60,11 +60,11 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        nn = int(n[1])
-        if nn < 4:
+        n = int(n[1])
+        if n < 4:
             print("N must be at least 4")
             sys.exit(1)
-        solutions = nqueens(nn)
+        solutions = nqueens(n)
         for solution in solutions:
             print(solution)
     except ValueError:
